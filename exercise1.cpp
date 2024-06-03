c495e4e48a1f55fd8e4d05192d63a87202f002ea
socurises <masha45603@gmail.com>
2024-06-03 23:21:42 +0300

    Create exercise1.cpp

---
+++ exercise1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int num1, num2;
+
+    // Ввод первого числа
+    std::cout << "Введите первое целое число: ";
+    std::cin >> num1;
+
+    // Ввод второго числа
+    std::cout << "Введите второе целое число: ";
+    std::cin >> num2;
+
+    // Сложение
+    int sum = num1 + num2;
+    std::cout << "Сумма " << num1 << " и " << num2 << " ровна: " << sum << std::endl;
+
+    // Вычитание
+    int difference = num1 - num2;
+    std::cout << "Разница " << num1 << " и " << num2 << " ровна: " << difference << std::endl;
+
+    return 0;
+}
